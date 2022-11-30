99041590b24d5974f1b2816400ed850869043898
pazcalll <yazeedarifin@gmail.com>
2022-11-30 13:12:28 +0700

    api comment edit

--- routes/api.go
+++ routes/api.go
@@ -17,7 +17,7 @@ func Init() *echo.Echo {
 	e.POST("/upload/book", admin.BookUpload)       // menambahkan buku dengan name (string), author (string), img (file: .png, .jpg, .jpeg) sebagai param
 	e.PUT("/return", admin.ReturnBook)             // mengembalikan buku yang dipinjam dengan id (int) sebagai param
 	e.GET("/borrow-report", admin.GetBorrowReport) // mendapatkan laporan rekap semua buku yang dipinjam
-	e.GET("/return-detail", admin.GetReturnDetail) // mendapat informasi detail dari buku yang dipinjam dengan parameter id (int)
+	e.GET("/return-detail", admin.GetReturnDetail) // mendapat informasi detail dari buku yang dipinjam dengan parameter borrow_id (int)
 	e.GET("/book-all", admin.BookAll)              // mendapat semua data buku dari database ditambah foto
 	e.GET("/user-all", admin.UserAll)              // mendatap semua data user dari database ditambah foto
 
